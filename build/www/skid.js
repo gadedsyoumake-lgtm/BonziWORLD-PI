747e0e7229d1fb99339a7fe4695798af6ca083d3
ItzRafaelScout <rafaelaltrafaelduartealt@gmail.com>
2026-01-05 11:54:36 -0500

    Update skid.js

--- build/www/skid.js
+++ build/www/skid.js
@@ -1,17 +1,1 @@
-document.addEventListener('keydown', (e) => {
-    // F12 key
-    if (e.key === 'F12' || e.keyCode === 123) {
-      e.preventDefault();
-      window.location.href = "skid.html";
-    }
-    // Ctrl+Shift+I for Windows/Linux
-    if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.keyCode === 73)) {
-      e.preventDefault();
-      window.location.href = "skid.html";
-    }
-    // Cmd+Opt+I for Mac
-    if (e.metaKey && e.altKey && (e.key === 'I' || e.keyCode === 73)) {
-      e.preventDefault();
-      window.location.href = "skid.html";
-    }
-  });
+// moved
